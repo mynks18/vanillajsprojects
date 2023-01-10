3f8f7c1803438c3e996ab5cd9e817be18ef63278
Mayank Sharma <mayankmssharma18@gmail.com>
2023-01-10 13:37:01 +0530

    seats counts and toggle added in script.js

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = movieSelect.value;
+const ticketPrice = +movieSelect.value;
 
-console.log("ok");
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
